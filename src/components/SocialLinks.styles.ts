7213d35a65b42839d334e8ade94f186c8e041baa
peterjonathang@gmail.com <peterjonathang@gmail.com>
2025-01-07 22:33:28 -0800

    [chore] switch from phosphor to fontawesome for icons

---
+++ src/components/SocialLinks.styles.ts
@@ -0,0 +1,10 @@
+import {css} from "@emotion/react";
+
+export const linksContainer = css`
+
+`
+
+export const icon = css`
+    width: 1.5rem;
+    height: 1.5rem;
+`;
---
+++ src/components/SocialLinks.tsx
@@ -0,0 +1,12 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react'
+
+import * as styles from './SocialLinks.styles';
+
+const SocialLinks = (): React.ReactElement => {
+    return <div css={styles.linksContainer}>
+
+    </div>
+}
+
+export default SocialLinks;

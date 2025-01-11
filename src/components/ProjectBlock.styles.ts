363df1b0a65631e5c18a05d1d5c96a823eb1ebea
peterjonathang@gmail.com <peterjonathang@gmail.com>
2025-01-10 21:49:44 -0800

    [feature] add basic projects section

--- src/components/ProjectBlock.styles.ts
+++ src/components/ProjectBlock.styles.ts
@@ -1,9 +1,79 @@
-import { css, SerializedStyles } from "@emotion/react";
+import { css } from "@emotion/react";
 
-export const blockContainer = css`
-    box-sizing: border-box;
+export const projectContainer = css`
+    border-radius: 8px;
     padding: 1rem 1.5rem;
+    display: grid;
+    grid-template-columns: 120px 1fr;
+    gap: 24px;
+    margin-bottom: 12px;
+    transition: background-color 0.2s ease;
+    cursor: default;
+
+    @media (min-width: 1025px) {
+        &:hover {
+            transform: scale(1.015);
+            background-color: rgba(255, 255, 255, 0.05);
+        }
+    }
+
+    @media (max-width: 1024px) {
+        grid-template-columns: 1fr;
+        gap: 8px;
+        padding: 0 0 3rem 0;
+    }
+`;
+
+export const thumbnailSection = css`
+    width: 125px;
+    height: 75px;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 12px;
+    border: 3px solid;
+    background-color: rgb(226 232 240);
+    filter: brightness(85%);
+`;
+
+export const thumbnail = css`
+    width: auto;
+    height: 100%;
+    object-fit: cover;
+`;
+
+export const contentSection = css`
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+`;
+
+export const projectName = css`
+    color: rgb(226 232 240);
+    margin: 0;
+    text-decoration: none;
+  
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+export const description = css`
+    font-size: 0.9rem;
+    line-height: 1.6;
+`;
+
+export const techPillsContainer = css`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 8px;
+`;
+
+export const techPill = css`
+    font-size: 0.75rem;
+    font-weight: 500;
+    padding: 4px 12px;
+    border-radius: 16px;
     border: 1px solid rgb(226 232 240);
-    border-radius: 6px;
-    margin-top: 1rem;
 `;
--- src/components/ProjectBlock.tsx
+++ src/components/ProjectBlock.tsx
@@ -2,9 +2,55 @@
 import React from "react"
 
 import * as styles from "./ProjectBlock.styles";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUpRightFromSquare} from "@fortawesome/free-solid-svg-icons";
 
-const ProjectBlock = (): React.ReactElement => {
-    return <div css={styles.blockContainer}>Project Block</div>
+interface ProjectBlockProps {
+    thumbnail: string;
+    projectTitle: string;
+    description: string;
+    technologies: string[];
+    projectUrl?: string;
+}
+
+const ProjectBlock: React.FC<ProjectBlockProps> = (props: ProjectBlockProps) => {
+
+    const {
+        thumbnail,
+        projectTitle,
+        description,
+        technologies,
+        projectUrl,
+    } = props;
+    
+    return (
+        <div
+            role="button"
+            tabIndex={0}
+            css={styles.projectContainer}>
+            <div css={styles.thumbnailSection}>
+                <img
+                    src={thumbnail}
+                    alt={`${projectTitle} thumbnail`}
+                    css={styles.thumbnail}
+                />
+            </div>
+
+            <div css={styles.contentSection}>
+                <div css={styles.projectName} onClick={projectUrl ? () => window.open(projectUrl, "_blank", "noopener,noreferrer") : () => {}}>
+                    {projectTitle} {projectUrl && <FontAwesomeIcon icon={faUpRightFromSquare} css={{marginLeft: "5px"}}/>}
+                </div>
+
+                <div css={styles.description}>{description}</div>
+
+                <div css={styles.techPillsContainer}>
+                    {technologies.map((tech, index) => (
+                        <div css={styles.techPill} key={index}>{tech}</div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default ProjectBlock;

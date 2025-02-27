7faa32c1f593ea4d964d98c618aef63cc07df7a4
peterjonathang@gmail.com <peterjonathang@gmail.com>
2025-02-26 17:40:48 -0800

    [fix] incorporate manifest in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,22 @@
-import type { AppProps } from "next/app";
-import "@fontsource/inter";
-import "@fontsource/inter/300.css";
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/600.css";
-import "@fontsource/inter/700.css";
+import Head from "next/head";
 import { Global } from "@emotion/react";
 import { globalStyles } from "../src/GlobalStyles";
+import { AppProps } from "next/app";
+import "@fontsource/inter";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
+            <Head>
+                <meta name="theme-color" content="#000000" />
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+                <link rel="manifest" href="/manifest.json" />
+                <meta
+                    name="description"
+                    content="Peter is a software developer from Vancouver, Canada. He loves helping organizations build ambitious yet scalable digital experiences."
+                />
+            </Head>
             <Global styles={globalStyles} />
             <Component {...pageProps} />
         </>
